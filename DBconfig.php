85972c41ba5fad92b43849ed129f83ead56457d4
SharonGreenbaum <92097635+SharonGreenbaum@users.noreply.github.com>
2022-05-23 15:46:59 +0300

    Create DBconfig.php

---
+++ DBconfig.php
@@ -0,0 +1,15 @@
+<?php
+ 
+ //Define your host here.
+ $HostName = "localhost";
+  
+ //Define your database name here.
+ $DatabaseName = "sharongr_Police_Israel";
+  
+ //Define your database username here.
+ $HostUser = "sharongr_sharon";
+  
+ //Define your database password here.
+ $HostPass = "12345";
+  
+ ?>
